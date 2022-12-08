27527a6fc9db4b914827fa636451dfb0cb6c9f91
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2022-12-08 21:27:17 +0500

    I have created a welcome screen for our game.

--- app.ts
+++ app.ts
@@ -1,1 +1,46 @@
-console.log("hello world")
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import gradient from "gradient-string";
+
+let sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 2000);
+    })
+}
+
+async function welcome() {
+    const rainbowTitle = chalkAnimation.rainbow(
+        `
+              Welcome to the guess the number game \n        
+        `
+    );
+    await sleep();
+    rainbowTitle.stop();
+
+
+    console.log(gradient.pastel.multiline(figlet.textSync("Guess the Number Game", { horizontalLayout: 'full' })))
+
+    let getUserName = await inquirer.prompt([{
+        name: "user_name",
+        type: 'string',
+        message: "hey! user, please enter your name: ",
+        default() {
+            return "Player-1";
+        },
+    }])
+
+
+    console.log('\n');
+    console.log(
+        `${chalk.greenBright(`Hello ${getUserName.user_name} \n`)}
+         ${chalk.cyan.bold(`You have three levels in this game ${chalk.blue.bold("1) easy")} ${chalk.blue.bold("2) medium")} and ${chalk.blue.bold("3) hard")}`)}          
+         ${chalk.hex('#FFA500').bold(`You have 6 chances to guess the correct number and ${chalk.green.bold("if you guess the right number you win.")}`)} 
+         ${chalk.hex('#FFA500').bold(`if you win the game cli will ask to do you want to continue? press y or n`)}
+         ${chalk.red.bold(`if you lose the game is over`)}         
+        `
+       );
+    }
+
+await welcome()
